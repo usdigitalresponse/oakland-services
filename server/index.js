320312d1d1a3a0d336cb1d63f0732478d5831605
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-07-01 14:57:53 +0800

    Server code cleanup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../build")));
+const CLIENT_PATH = path.join(__dirname, "../", "build");
+
+app.use(express.static(CLIENT_PATH));
 
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("../knexfile")[environment];
@@ -16,61 +18,61 @@ function getLang(req) {
   return lang;
 }
 
-app.get("/ping", function (_req, res) {
-  return res.send("pong");
+app.get("/ping", (_req, res) => {
+  res.send("pong");
 });
 
-app.get("/", function (_req, res) {
-  res.sendFile(path.join(__dirname, "../", "build", "index.html"));
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(CLIENT_PATH, "index.html"));
 });
 
-app.get("/categories", function (req, res) {
-  database("categories")
+app.get("/categories", async (req, res) => {
+  const categories = await database("categories")
     .join("category_details", "categories.id", "category_details.category_id")
-    .where({ "category_details.lang": getLang(req) })
-    .then(function (categories) {
-      res.status(200).json(categories);
-    });
+    .where({ "category_details.lang": getLang(req) });
+
+  res.status(200).json(categories);
 });
 
-app.get("/categories/:category_id/services", function (req, res) {
+app.get("/categories/:category_id/services", async (req, res) => {
   const categoryId = req.params.category_id;
 
-  database("services")
+  const services = database("services")
     .join("service_details", "services.id", "service_details.service_id")
     .join("categorizations", "services.id", "categorizations.service_id")
     .join("categories", "categorizations.category_id", "categories.id")
     .where({ category_id: categoryId })
-    .where({ "service_details.lang": getLang(req) })
-    .then(function (services) {
-      res.status(200).json(services);
-    });
+    .where({ "service_details.lang": getLang(req) });
+
+  res.status(200).json(services);
 });
 
-app.get("/services/:service_id", function (req, res) {
+app.get("/services/:service_id", async (req, res) => {
   const serviceId = req.params.service_id;
 
-  database("services")
+  const service = database("services")
     .join("service_details", "services.id", "service_details.service_id")
     .where({ "service_details.lang": getLang(req) })
     .where({ "services.id": serviceId })
-    .first()
-    .then(function (services) {
-      res.status(200).json(services);
-    });
+    .first();
+
+  res.status(200).json(service);
 });
 
-app.get("/neighborhoods", function (req, res) {
-  database("neighborhoods")
+app.get("/neighborhoods", async (req, res) => {
+  const neighborhoods = database("neighborhoods")
     .join(
       "neighborhood_details",
       "neighborhoods.id",
       "neighborhood_details.neighborhood_id"
     )
-    .where({ "neighborhood_details.lang": getLang(req) })
-    .then(function (neighborhoods) {
-      res.status(200).json(neighborhoods);
-    });
+    .where({ "neighborhood_details.lang": getLang(req) });
+
+  res.status(200).json(neighborhoods);
+});
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(CLIENT_PATH, "index.html"));
 });
 
 app.listen(process.env.PORT || 5000);
